fba8ddd0e89ba5b21ddcf713833fd9a3e359703e
Wainow <62390803+Wainow@users.noreply.github.com>
2021-11-13 11:37:16 +0400

    4.3 task
    4.3 task

--- main.py
+++ main.py
@@ -26,14 +26,34 @@ def check_matr_gol():
     return check
 #------------------------------------------------------------
 
+# -----------------------------4.3---------------------------
+def gen_G(r, m):
+    result = []
+    if(r == 0):
+        result = np.random.randint(1, 2, (1, pow(2, m)))
+    if(r == m):
+        string = np.append(np.zeros(pow(2,m)-1), np.array(1))
+        result = np.vstack((G(m-1, m), string))
+    if(r > 0 and r < m):
+        zeros = np.random.randint(0, 1, (1, pow(2, m-1)))
+        string1 = np.hstack((G(r, m-1), G(r, m-1)))
+        g = G(r-1, m-1)
+        string2 = np.hstack((np.zeros(g.shape),g))
+        result = np.vstack((string1, string2))
+    return result
 
-# -----------------------------4.2---------------------------
+def check_G(r, m):
+    eye = np.eye(pow(2,m))
+    g = G(r, m)
+    return np.concatenate((eye, g))
+#------------------------------------------------------------
 
 
 if __name__ == '__main__':
     #-------print 4.1----------
     #print(gen_matr_gol())
     #print(check_matr_gol())
-    #-------print 4.2----------
-
+    #-------print 4.3----------
+    #print(gen_G(3,4)
+    #print(check_G(3,4)
 
